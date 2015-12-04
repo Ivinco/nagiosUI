1c2755deef6a32173efa877e5ff148d690184ed4
Marius Matilionis marius@ivinco.com <marius@ivinco.com>
2015-12-04 05:07:02 -0500

    fix

--- htdocs/json.php
+++ htdocs/json.php
@@ -8,26 +8,27 @@ $array      = json_decode(json_encode(simplexml_load_string(returnDataList(false
 $returnJson = array();
 
 foreach ($array['alert'] as $item) {
-	$acked      = (!empty($item['acked']))                ? $item['acked']                : '';
-	$ackComment = (!empty($item['ack_comment']))          ? $item['ack_comment']          : '';
-	$ackAuthor  = (!empty($item['ack_author']))           ? $item['ack_author']           : '';
-	$ackComDate = (!empty($item['ack_comment_date']))     ? $item['ack_comment_date']     : '';
-	$sched      = (!empty($item['sched']))                ? $item['sched']                : '';
-	$schComment = (!empty($item['sched_comment']))        ? $item['sched_comment']        : '';
-	$schAuthor  = (!empty($item['sched_author']))         ? $item['sched_author']         : '';
-	$schComDate = (!empty($item['sched_comment_date']))   ? $item['sched_comment_date']   : '';
-	$host       = (!empty($item['host']))                 ? $item['host']                 : '';
-	$hostUrl    = (!empty($item['host-url']))             ? $item['host-url']             : '';
-	$service    = (!empty($item['service']))              ? $item['service']              : '';
-	$serviceUrl = (!empty($item['service-url']))          ? $item['service-url']          : '';
-	$notesUrl   = (!empty($item['notes_url']))            ? $item['notes_url']            : '';
-	$state      = (!empty($item['@attributes']['state'])) ? $item['@attributes']['state'] : '';
-	$downtimeId = (!empty($item['downtime_id']))          ? $item['downtime_id']          : '';
-	$lastCheck  = (!empty($item['last_check']))           ? $item['last_check']           : '';
-	$lastCheckS = (!empty($item['last_check_sec']))       ? $item['last_check_sec']       : '';
-	$duration   = (!empty($item['duration']))             ? $item['duration']             : '';
-	$durationS  = (!empty($item['durationSec9Digits']))   ? $item['durationSec9Digits']   : '';
-	$statusInfo = (!empty($item['status_information']))   ? $item['status_information']   : '';
+	$acked      = (!is_array($item['acked']))                ? $item['acked']                : implode(' ', $item['acked']);
+	$ackComment = (!is_array($item['ack_comment']))          ? $item['ack_comment']          : implode(' ', $item['ack_comment']);
+	$ackAuthor  = (!is_array($item['ack_author']))           ? $item['ack_author']           : implode(' ', $item['ack_author']);
+	$ackComDate = (!is_array($item['ack_comment_date']))     ? $item['ack_comment_date']     : implode(' ', $item['ack_comment_date']);
+	$sched      = (!is_array($item['sched']))                ? $item['sched']                : implode(' ', $item['sched']);
+	$schComment = (!is_array($item['sched_comment']))        ? $item['sched_comment']        : implode(' ', $item['sched_comment']);
+	$schAuthor  = (!is_array($item['sched_author']))         ? $item['sched_author']         : implode(' ', $item['sched_author']);
+	$schComDate = (!is_array($item['sched_comment_date']))   ? $item['sched_comment_date']   : implode(' ', $item['sched_comment_date']);
+	$host       = (!is_array($item['host']))                 ? $item['host']                 : implode(' ', $item['host']);
+	$hostUrl    = (!is_array($item['host-url']))             ? $item['host-url']             : implode(' ', $item['host-url']);
+	$service    = (!is_array($item['service']))              ? $item['service']              : implode(' ', $item['service']);
+	$serviceUrl = (!is_array($item['service-url']))          ? $item['service-url']          : implode(' ', $item['service-url']);
+	$notesUrl   = (!is_array($item['notes_url']))            ? $item['notes_url']            : implode(' ', $item['notes_url']);
+	$state      = (!is_array($item['@attributes']['state'])) ? $item['@attributes']['state'] : implode(' ', $item['@attributes']['state']);
+	$downtimeId = (!is_array($item['downtime_id']))          ? $item['downtime_id']          : implode(' ', $item['downtime_id']);
+	$lastCheck  = (!is_array($item['last_check']))           ? $item['last_check']           : implode(' ', $item['last_check']);
+	$lastCheckS = (!is_array($item['last_check_sec']))       ? $item['last_check_sec']       : implode(' ', $item['last_check_sec']);
+	$duration   = (!is_array($item['duration']))             ? $item['duration']             : implode(' ', $item['duration']);
+	$durationS  = (!is_array($item['durationSec9Digits']))   ? $item['durationSec9Digits']   : implode(' ', $item['durationSec9Digits']);
+	$statusInfo = (!is_array($item['status_information']))   ? $item['status_information']   : implode(' ', $item['status_information']);
+	
 	$returnType = '';
 	$returnType.= (($acked == 0 && $sched == 0) || ($acked == 1 && $ackComment == 'temp')) ? '__normal__' : '';
 	$returnType.= ($acked == 1 && $ackComment != 'temp') ? '__acked__' : '';
