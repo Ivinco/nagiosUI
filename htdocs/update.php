47f386d85c6d2fad9f678be4614288f43ad76a60
Marius Matilionis marius@ivinco.com <marius@ivinco.com>
2016-04-21 06:13:03 -0400

    update.php fix

--- htdocs/update.php
+++ htdocs/update.php
@@ -2,17 +2,30 @@
 
 set_time_limit(0);
 
+if (!isset($_GET['hash']) || !$_GET['hash']) {
+    http_response_code(404);
+    die;
+}
+
+$counter = 0;
+
 include_once 'functions.php';
 
 while (true) {
+    if ($counter > 120) {
+        http_response_code(408);
+        break;
+    }
+    
     clearstatcache();
-    $lastFileHash = returnDataList(true, false); 
+    $lastFileHash = returnDataList(true, false);
 
-    if (isset($_GET['hash']) && $lastFileHash != $_GET['hash']) {
+    if ($lastFileHash != $_GET['hash']) {
         echo $lastFileHash;
         break;
     } else {
         sleep(1);
+        $counter++;
         continue;
     }
 }
