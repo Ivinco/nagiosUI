292b778ae1b6d9b17b63fc423387a13148cdb0be
Marius Matilionis marius@ivinco.com <marius@ivinco.com>
2017-08-03 08:41:02 -0400

    planned fix

--- scripts/planned.php
+++ scripts/planned.php
@@ -164,7 +164,7 @@ class planned
 
             foreach ($hostCommands as $commandHost) {
                 foreach ($serviceCommands as $commandService) {
-                    if (preg_match("/$commandHost/iu", " " . $host . " ") && preg_match("/$commandService/iu", " " . $service . " ") && $plan['end'] > time()) {
+                    if (preg_match("/$commandHost/iu", $host) && preg_match("/$commandService/iu", " " . $service . " ") && $plan['end'] > time()) {
                         $type = (isset($plan['list']) && isset($plan['list'][$host]) && isset($plan['list'][$host][$service])) ? 'old' : 'new';
 
                         return [
