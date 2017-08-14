60fdac3cf8a222dfc8734c360f8fdfd6a5ef8aea
Marius Matilionis marius@ivinco.com <marius@ivinco.com>
2017-08-14 10:55:52 -0400

    xml fix

--- htdocs/xml.php
+++ htdocs/xml.php
@@ -5,6 +5,8 @@ include_once __DIR__ . '/../scripts/init.php';
 ob_start('ob_gzhandler');
 header("Content-Type: application/xml");
 
+$xmlFile = (isset($_GET['file'])) ? $_GET['file'] : '';
+
 $xml = new xml;
 
-echo $xml->returnXml(false, false);
+echo $xml->returnXml(false, $xmlFile);
