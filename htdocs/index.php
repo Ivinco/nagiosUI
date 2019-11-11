6be0787980e1a224e54b602fe9f175aacf363b8d
Marius Matilionis marius@ivinco.com <marius@ivinco.com>
2019-11-11 01:36:22 -0500

    small bugfixes

--- htdocs/index.php
+++ htdocs/index.php
@@ -75,7 +75,12 @@
 </div>
 
 <div id="noData" style="display: none;">
-    <h1>Render error: Can't get data for server. You can try clear 'localStorage' by clicking: <span id="clearLocalStorage" style="cursor: pointer; text-decoration: underline;">here</span></h1>
+    <h2>Server error occurred: Can't get data for rendering. You can try to:</h2>
+    <ol>
+        <li style="font-size: 13px;">reload page</li>
+        <li style="font-size: 13px;">clear 'localStorage' by clicking: <span id="clearLocalStorage" style="cursor: pointer; text-decoration: underline;  font-size: 13px;">here</span> (it will delete 'localStorage' only for this site)</li>
+        <li style="font-size: 13px;">report issue in <a href="https://github.com/Ivinco/nagiosUI/issues" target="_blank" style="font-size: 13px;">github.com</a></li>
+    </ol>
 </div>
 <div id="infoHolder" style="display: none;">
     <div id="tabs">
@@ -224,7 +229,13 @@
 	<div id="commentDialog" title="Comment"></div>
 	<div id="plannedDialog" title="Planned Templates"></div>
     <div id="noData1" style="display: none;">
-        <h1>Render error: Can't get data for server. You can try clear 'localStorage' by clicking: <span id="clearLocalStorage" style="cursor: pointer; text-decoration: underline;">here</span> or select another server.</h1>
+        <h2>Server error occurred: Can't get data for rendering. You can try to:</h2>
+        <ol>
+            <li style="font-size: 13px;">select another server</li>
+            <li style="font-size: 13px;">reload page</li>
+            <li style="font-size: 13px;">clear 'localStorage' by clicking: <span id="clearLocalStorage" style="cursor: pointer; text-decoration: underline;  font-size: 13px;">here</span> (it will delete 'localStorage' only for this site)</li>
+            <li style="font-size: 13px;">report issue in <a href="https://github.com/Ivinco/nagiosUI/issues" target="_blank" style="font-size: 13px;">github.com</a></li>
+        </ol>
     </div>
 </div>
 
